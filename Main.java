92e0265eab960ec906501410a3c83e8cba90538c
eugeniaaaaaa <45148607+eugeniaaaaaa@users.noreply.github.com>
2020-06-02 10:32:45 +0300

    Lab 8

--- Main.java
+++ Main.java
@@ -1,18 +1,17 @@
 package com.company;
 
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-
 public class Main {
 
-    public static void main(String[] args) throws IOException {
-        String path="/home/yarik/Загрузки/Copy.zip (Unzipped Files)/Copy/Laba6/";
-        FileReader fileReader = new FileReader(path+"original.text");
-        FileWriter fileWriter = new FileWriter(path+"encrypted.text");
-        FileReader fileReader1 = new FileReader(path+"encrypted.text");
-        FileWriter fileWriter1 = new FileWriter(path+"decrypted_text.text");
-        ThreadCipher.encryptDate(fileReader,fileWriter);
-        ThreadCipher.decreptedDate(fileReader1, fileWriter1);
+    public static void main(String[] args) {
+        System.out.println(Bank.calculateAllMoney());
+        for (int itter = 0; itter < 1000; itter++) {
+            new TransactionThread().start();
+        }
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println(Bank.calculateAllMoney());
     }
 }
